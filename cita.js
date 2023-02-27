337c93291acbedda77b8afddff2c96490bf23ea7
Martzom <82251202+martzom@users.noreply.github.com>
2023-02-26 20:40:07 -0600

    cambios finales

---
+++ cita.js
@@ -0,0 +1,22 @@
+$("#Presencial").click(function(){
+    $("#direccion").css("display", "none");
+});
+
+
+$("#Adomicilio").click(function(){
+    $("#direccion").css("display", "block");
+});
+
+
+$(".Delete").click(function(){
+   alert("Se elimino correctamente");
+});
+
+$("#ConsultaPresencial").click(function(){
+    $("#domicilio").css("display", "none");
+});
+
+
+$("#ConsultaAdomicilio").click(function(){
+    $("#domicilio").css("display", "block");
+});
